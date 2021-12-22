c7c4a01760c110aa4101a2bf49d89ca40af65693
THEO-184 <theophilusboakye47@gmail.com>
2021-12-22 08:17:24 -0800

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
 			<div className="logo-container">
 				<Avatar src={icon} className="logo" size="large" />
 				<Typography.Title level={2} className="logo">
-					<Link to="/">CryptoWorld</Link>
+					<Link to="/">Theo Crypto</Link>
 				</Typography.Title>
 				<Button
 					className="menu-control-container"
